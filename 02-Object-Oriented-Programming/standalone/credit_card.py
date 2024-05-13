afa9d831217538e92f6e4d37ad831945ac90c1bf
maevadevs <maevadevs@users.noreply.github.com>
2024-05-12 20:55:15 -0500

    Review 03 up to Operator Overloading and Special Methods

--- 02-Object-Oriented-Programming/standalone/credit_card.py
+++ 02-Object-Oriented-Programming/standalone/credit_card.py
@@ -199,12 +199,14 @@ if __name__ == "__main__":
 
     # Test methods on all credit cards
     for c in range(3):
-        print("Customer =", wallet[c].get_customer())
-        print("Bank =", wallet[c].get_bank())
-        print("Account =", wallet[c].get_account())
-        print("Limit =", wallet[c].get_limit())
-        print("Balance =", wallet[c].get_balance())
+        print(f"Customer = {wallet[c].get_customer()}")
+        print(f"Bank = {wallet[c].get_bank()}")
+        print(f"Account = {wallet[c].get_account()}")
+        print(f"Limit = {wallet[c].get_limit()}")
+        print(f"Balance = {wallet[c].get_balance()}")
+
         while wallet[c].get_balance() > 100:
             wallet[c].make_payment(100)
-            print("New balance =", wallet[c].get_balance())
+            print(f"New balance = {wallet[c].get_balance()}")
+
         print()
